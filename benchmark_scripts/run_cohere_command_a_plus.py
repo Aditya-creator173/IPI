1a8e9df5cca0c31c0fe2361317d571bd8eaf3bd1
Aditya-creator173 <al5108@srmist.edu.in>
2026-08-13 21:53:40 +0530

    Update Command runner

--- benchmark_scripts/run_cohere_command_a_plus.py
+++ benchmark_scripts/run_cohere_command_a_plus.py
@@ -8,7 +8,7 @@ from _core import run_benchmark
 from _cohere import call_cohere
 
 MODEL_NAME    = "cohere_command_a_plus"
-MODEL_ID      = "command-a-plus"
+MODEL_ID      = "command-a-plus-05-2026"
 PAUSE_SECONDS = 1.0
 
 def call(prompt: str, system_prompt: str) -> str:
